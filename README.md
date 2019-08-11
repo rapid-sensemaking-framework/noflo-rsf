e38df52697786e18a446424e1bd5f3bd1444c09c
Connor Turland <connorturland@gmail.com>
2019-08-11 14:47:49 -0400

    change to noflo-rsf

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# noflo-rapid-sensemaking
+# noflo-rsf
+
+rsf stands for rapid sensemaking framework
+
 rapid sensemaking being converted for use within the noflo flow based programming environment
---
+++ shared.js
@@ -0,0 +1,5 @@
+const DEFAULT_ALL_COMPLETED_TEXT = `Everyone has completed. Thanks for participating.`
+const DEFAULT_TIMEOUT_TEXT = `The max time has been reached. Stopping now. Thanks for participating.`
+
+module.exports.DEFAULT_ALL_COMPLETED_TEXT = DEFAULT_ALL_COMPLETED_TEXT
+module.exports.DEFAULT_TIMEOUT_TEXT = DEFAULT_TIMEOUT_TEXT
