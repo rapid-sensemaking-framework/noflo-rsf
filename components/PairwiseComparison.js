7cb7c4be5e9d53a4552ee8dec4c938fbc2bcc7f7
Connor Turland <connorturland@gmail.com>
2019-08-12 07:45:17 -0400

    finished PairwiseComparison initial pass

--- components/PairwiseComparison.js
+++ components/PairwiseComparison.js
@@ -11,10 +11,11 @@ const {
     DEFAULT_TIMEOUT_TEXT
 } = require('../shared')
 
-const DEFAULT_MAX_RESPONSES_TEXT = `You've reached the limit of responses. Thanks for participating. You will be notified when everyone has completed.`
-const rulesText = (maxTime, maxResponses) => 'Contribute one response per message. ' +
-    `You can contribute up to ${maxResponses} responses. ` +
-    `The process will stop automatically after ${maxTime / 1000} seconds.`
+// define constants or creator functions
+// of the strings for user interaction here
+const DEFAULT_INVALID_RESPONSE_TEXT = `That's not a valid response, please try again.`
+const DEFAULT_MAX_RESPONSES_TEXT = `You've responded to everything. Thanks for participating. You will be notified when everyone has completed.`
+const rulesText = (maxTime) => `The process will stop automatically after ${maxTime} milliseconds.` // TODO: improve the human readable time
 
 // a value that will mean any amount of responses can be collected
 // from each person, and that the process will guaranteed last until the maxTime comes to pass
@@ -23,18 +24,19 @@ const UNLIMITED_CHAR = '*'
 const process = (input, output) => {
 
     // Check preconditions on input data
-    if (!input.hasData('prompt', 'contactable_configs', 'max_time', 'bot_configs')) {
+    if (!input.hasData('choice', 'statements', 'max_time', 'contactable_configs', 'bot_configs')) {
         return
     }
 
     // Read packets we need to process
-    const maxResponses = input.getData('max_responses')
+    const choice = input.getData('choice')
+    const statements = input.getData('statements')
     const maxTime = input.getData('max_time')
-    const prompt = input.getData('prompt')
     const botConfigs = input.getData('bot_configs')
     const contactableConfigs = input.getData('contactable_configs')
     const maxResponsesText = input.getData('max_responses_text')
     const allCompletedText = input.getData('all_completed_text')
+    const invalidResponseText = input.getData('invalid_response_text')
     const timeoutText = input.getData('timeout_text')
 
     let contactables
@@ -51,12 +53,13 @@ const process = (input, output) => {
         return
     }
 
-    if (!maxResponses || maxResponses === UNLIMITED_CHAR) {
-        maxResponses = Infinity
-    }
-
     // array to store the results
     const results = []
+
+    // number of results to expect, per contactable
+    // the number of possible pairs
+    const n = statements.length
+    const numPerPerson = n * (n - 1) / 2
 
     // stop the process after a maximum amount of time
     const timeoutId = setTimeout(() => {
@@ -69,6 +72,7 @@ const process = (input, output) => {
     let calledComplete = false
     const complete = (completionText) => {
         if (!calledComplete) {
+            // It is good practice to inform participants the process is ending
             contactables.forEach(contactable => contactable.speak(completionText))
             clearTimeout(timeoutId)
             // Process data and send output
@@ -81,38 +85,86 @@ const process = (input, output) => {
         }
     }
 
+    // a function to check the validity of a response
+    // according to the options
+    const validResponse = (text) => {
+        return text === '1' || text === 'A'
+    }
+
+    // a function to check the completion conditions
+    const checkCompletionCondition = () => {
+        // exit when everyone has responded to everything
+        if (results.length === contactables.length * numPerPerson) {
+            complete(allCompletedText || DEFAULT_ALL_COMPLETED_TEXT)
+        }
+    }
+
+    // create a list of all the pairs
+    const pairsTexts = []
+    statements.forEach((statement, index) => {
+        for (let i = index + 1; i < statements.length; i++) {
+            let pairedStatement = statements[i]
+            // use A and 1 to try to minimize preference
+            // bias for 1 vs 2, or A vs B
+            pairsTexts.push({
+                A: statement.text,
+                1: pairedStatement.text
+            })
+        }
+    })
+
+    // The "rules" of the game should be conveyed here
+    // Make sure people fully understand the process
     contactables.forEach(contactable => {
-        // keep track of the number of responses from this person
-        let responseCount = 0
 
         // initiate contact with the person
         // and set context, and "rules"
-        contactable.speak(prompt)
-        setTimeout(() => contactable.speak(rulesText(maxTime, maxResponses)), 500)
+        // contactable.speak(prompt)
+        contactable.speak(rulesText(maxTime))
+        setTimeout(() => {
+            contactable.speak(choice)
+        }, 500)
 
-        // listen for messages from them, and treat each one
-        // as an input, up till the alotted amount
+        // send them one message per pair,
+        // awaiting their response before sending the next
+        let responseCount = 0
+        const nextText = () => {
+            return `(${numPerPerson - 1 - responseCount} remaining) A) ${pairsTexts[responseCount]['A']} 1) ${pairsTexts[responseCount]['1']}`
+        }
         contactable.listen(text => {
-            if (responseCount < maxResponses) {
+            // do we still accept this response?
+            if (responseCount < numPerPerson) {
+                if (!validResponse(text)) {
+                    contactable.speak(invalidResponseText || DEFAULT_INVALID_RESPONSE_TEXT)
+                    return
+                }
                 results.push({
-                    text,
+                    choices: pairsTexts[responseCount],
+                    choice: text,
                     id: contactable.id,
                     timestamp: Date.now()
                 })
                 responseCount++
             }
-            // in the case where maxResponses is Infinity,
-            // this will never match
-            if (responseCount === maxResponses) {
+
+            // is there anything else we should say?
+            if (responseCount === numPerPerson) {
+                // remind them they've responded to everything
                 contactable.speak(maxResponsesText || DEFAULT_MAX_RESPONSES_TEXT)
+            } else {
+                // still haven't reached the end,
+                // so send the next one
+                contactable.speak(nextText())
             }
-            // exit when everyone has added all their alotted responses
-            // in the case where maxResponses is Infinity,
-            // this will never match
-            if (results.length === contactables.length * maxResponses) {
-                setTimeout(() => complete(allCompletedText || DEFAULT_ALL_COMPLETED_TEXT), 500)
-            }
+
+            // are we done?
+            checkCompletionCondition()
         })
+        // send the first one
+        setTimeout(() => {
+            contactable.speak(nextText())
+        }, 1000)
+
     })
 }
 
@@ -120,22 +172,23 @@ exports.getComponent = () => {
     const c = new noflo.Component()
 
     /* META */
-    c.description = 'For a prompt, collect statements numbering up to a given maximum (or unlimited) from a list of participants'
+    c.description = 'Iterate through all the combinations in a list of statements getting peoples choices on them'
     c.icon = 'compress'
 
     /* IN PORTS */
-    c.inPorts.add('max_responses', {
-        datatype: 'all',
-        description: 'the number of responses to stop collecting at, don\'t set or use "*" for any amount'
+    c.inPorts.add('choice', {
+        datatype: 'string',
+        description: 'a human readable string clarifying what a choice for either of any two options means',
+        required: true
+    })
+    c.inPorts.add('statements', {
+        datatype: 'array',
+        description: 'the list of statements (as objects with property "text") to create all possible pairs out of, and make choices between',
+        required: true
     })
     c.inPorts.add('max_time', {
         datatype: 'int',
         description: 'the number of milliseconds to wait until stopping this process automatically',
-        required: true
-    })
-    c.inPorts.add('prompt', {
-        datatype: 'string',
-        description: 'the text that prompts people, and sets the rules and context',
         required: true
     })
     c.inPorts.add('contactable_configs', {
@@ -156,14 +209,29 @@ exports.getComponent = () => {
         datatype: 'string',
         description: 'msg override: the message sent to all participants when the process completes, by completion by all participants'
     })
+    c.inPorts.add('invalid_response_text', {
+        datatype: 'string',
+        description: 'msg override: the message sent when participant use an invalid response'
+    })
     c.inPorts.add('timeout_text', {
         datatype: 'string',
         description: 'msg override: the message sent to all participants when the process completes because the timeout is reached'
     })
 
     /* OUT PORTS */
+
     c.outPorts.add('results', {
         datatype: 'array'
+        /*
+        RESULTS:
+        [Choice], array of Choice
+        Choice.choices : ChoiceObject, the choices being chosen between
+        ChoiceObject.A : String, the text of the choice associated with key A
+        ChoiceObject.1 : String, the text of the choice associated with key 1
+        Choice.choice : String, 1 or A, whichever was chosen
+        Choice.id : String, the id of the contactable who chose
+        Choice.timestamp : Number, the unix timestamp specifying when the choice was made
+        */
     })
     c.outPorts.add('error', {
         datatype: 'all'
