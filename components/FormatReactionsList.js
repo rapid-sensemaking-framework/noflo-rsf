8b77846deb9f4d5016e017d130812ccbddef6497
Connor Turland <connorturland@gmail.com>
2019-08-14 23:59:54 -0400

    three new components for sending messages to all and formatting data

---
+++ components/FormatReactionsList.js
@@ -0,0 +1,55 @@
+const noflo = require('noflo')
+
+const process = (input, output) => {
+
+    // Check preconditions on input data
+    if (!input.hasData('reactions')) {
+        return
+    }
+
+    // Read packets we need to process
+    /*
+        [Response], array of the responses collected
+        Response.statement : Statement, the same as the Statement objects given
+        Response.response : String, the text of the response
+        Response.id : String, the id of the agent who gave the response
+        Response.timestamp : Number, the unix timestamp of the moment the message was received
+        */
+    const reactions = input.getData('reactions')
+
+    const formatted = reactions.reduce((memo, r) => {
+        return `${memo}
+${r.statement.text} : ${r.response} : ${r.id}`
+    }, '')
+
+    output.send({
+        formatted
+    })
+    output.done()
+}
+
+exports.getComponent = () => {
+    const c = new noflo.Component()
+
+    /* META */
+    c.description = 'Format a list of reactions to statements to a single string message'
+    c.icon = 'compress'
+
+    /* IN PORTS */
+    c.inPorts.add('reactions', {
+        datatype: 'array',
+        description: 'the list of reactions to format',
+        required: true
+    })
+
+    /* OUT PORTS */
+    c.outPorts.add('formatted', {
+        datatype: 'string'
+    })
+
+    /* DEFINE PROCESS */
+    c.process(process)
+
+    /* return */
+    return c
+}
---
+++ components/SendMessageToAll.js
@@ -0,0 +1,71 @@
+const noflo = require('noflo')
+const { init: contactableInit, makeContactable } = require('rsf-contactable')
+
+const process = (input, output) => {
+
+    // Check preconditions on input data
+    if (!input.hasData('message', 'contactable_configs', 'bot_configs')) {
+        return
+    }
+
+    // Read packets we need to process
+    const message = input.getData('message')
+    const botConfigs = input.getData('bot_configs')
+    const contactableConfigs = input.getData('contactable_configs')
+
+    let contactables
+    try {
+        contactableInit(botConfigs.mattermostable, botConfigs.textable)
+        contactables = contactableConfigs.map(makeContactable)
+    } catch (e) {
+        // Process data and send output
+        output.send({
+            error: e
+        })
+        // Deactivate
+        output.done()
+        return
+    }
+            
+    contactables.forEach(contactable => {
+        contactable.speak(message)
+    })
+    // Deactivate
+    output.done()
+}
+
+exports.getComponent = () => {
+    const c = new noflo.Component()
+
+    /* META */
+    c.description = 'Send a message to a list of people'
+    c.icon = 'compress'
+
+    /* IN PORTS */
+    c.inPorts.add('message', {
+        datatype: 'string',
+        description: 'the message to send',
+        required: true
+    })
+    c.inPorts.add('contactable_configs', {
+        datatype: 'array',
+        description: 'an array of rsf-contactable compatible config objects',
+        required: true
+    })
+    c.inPorts.add('bot_configs', {
+        datatype: 'object',
+        description: 'an object of rsf-contactable compatible bot config objects',
+        required: true
+    })
+
+    /* OUT PORTS */
+    c.outPorts.add('error', {
+        datatype: 'all'
+    })
+
+    /* DEFINE PROCESS */
+    c.process(process)
+
+    /* return */
+    return c
+}
