5fbddc6fe35708a0a27c7106765b72202df54646
Connor Turland <connorturland@gmail.com>
2019-11-08 20:25:31 -0500

    shared formatting

--- shared.js
+++ shared.js
@@ -6,21 +6,21 @@ const DEFAULT_MAX_RESPONSES_TEXT = `You've responded to everything. Thanks for p
 const rulesText = (maxTime) => `The process will stop automatically after ${maxTime} seconds.`
 
 const whichToInit = (contactableConfigs) => {
-    return contactableConfigs.reduce((memo, value) => {
-        const uppercased = value.type.charAt(0).toUpperCase() + value.type.slice(1)
-        memo[`do${uppercased}`] = true
-        return memo
-    }, {})
+  return contactableConfigs.reduce((memo, value) => {
+    const uppercased = value.type.charAt(0).toUpperCase() + value.type.slice(1)
+    memo[`do${uppercased}`] = true
+    return memo
+  }, {})
 }
 
 const timer = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 module.exports = {
-    DEFAULT_ALL_COMPLETED_TEXT,
-    DEFAULT_INVALID_RESPONSE_TEXT,
-    DEFAULT_MAX_RESPONSES_TEXT,
-    DEFAULT_TIMEOUT_TEXT,
-    rulesText,
-    whichToInit,
-    timer
+  DEFAULT_ALL_COMPLETED_TEXT,
+  DEFAULT_INVALID_RESPONSE_TEXT,
+  DEFAULT_MAX_RESPONSES_TEXT,
+  DEFAULT_TIMEOUT_TEXT,
+  rulesText,
+  whichToInit,
+  timer
 }
